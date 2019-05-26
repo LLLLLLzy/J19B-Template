bce79c7b9ba4373b128773d4fc459aa7341276da
lzy <lzy122112@foxmail.com>
2019-05-26 11:25:40 +0800

    First commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'J19B' 
+ * Target:  'J19B' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ README.md
@@ -0,0 +1,7 @@
+<<<<<<< HEAD
+# J19B-Template
+J19B项目模板
+=======
+# J19B_Template
+该项目为J19B程序模板
+>>>>>>> First push
